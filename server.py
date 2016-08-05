64fb9525d8bffd7bc3ddfc7889a6d33bf621e662
Hackbright Student <info@hackbrightacademy.com>
2016-08-04 17:55:44 -0700

    Implemented Judgy Eye.

--- server.py
+++ server.py
@@ -89,13 +89,68 @@ def movie_details(movie_id):
     if (not user_rating) and user_id:
         user = User.query.get(user_id)
         if user:
-            prediction = round(user.predict_rating(movie),2)
+            prediction = user.predict_rating(movie)
+
+    # Generate an effective rating to compare against The Eye's rating.
+    if prediction:
+        effective_rating = prediction
+
+    elif user_rating:
+        effective_rating = user_rating.score
+
+    else:
+        effective_rating = None
+
+
+    # Get the eye's rating, either by predicting it or using the real rating.
+    the_eye = User.query.get(666)
+
+    eye_rating = Rating.query.filter_by(
+                 user_id=the_eye.user_id,
+                 movie_id=movie.movie_id).first()
+
+    if eye_rating is None:
+        eye_rating = the_eye.predict_rating(movie)
+
+    else: 
+        eye_rating = eye_rating.score
+
+    print eye_rating
+
+    if eye_rating and effective_rating:
+        difference = abs(eye_rating - effective_rating)
+
+    else:
+        difference = None
+
+    # Choose a message based on how large the difference between ratings
+    # was.
+
+    BERATEMENT_MESSAGES = [
+        "I suppose you don't have such bad taste after all.",
+        "I regret every decision that I've ever made that has " +
+            "brought me to listen to your opinion.",
+        "Words fail me, as your taste in movies has clearly " +
+            "failed you.",
+        "That movie is great. For a clown to watch. Idiot.",
+        "Words cannot express the awfulness of your taste."
+        ]   
+
+    if difference is not None:
+        beratement = BERATEMENT_MESSAGES[int(difference)]
+
+    else:
+        beratement = None
+
+    if prediction:
+        prediction = round(prediction, 2)
 
     return render_template("movie_details.html",
                             movie=movie,
                             user_rating=user_rating,
                             average=avg_rating,
-                            prediction=prediction)
+                            prediction=prediction,
+                            beratement=beratement)
 
 ####################################################
 # Rating routes
